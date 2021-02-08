5dace413ac06445c72b801c2550c1c290bc75206
Patricia Maceiras Varela <pmaceirasvarela@danielcastelao.org>
2021-02-08 11:03:00 +0100

    exemplo matrices

--- src/com/exemplos2/exemplomatriz/MetodosMatrices.java
+++ src/com/exemplos2/exemplomatriz/MetodosMatrices.java
@@ -3,25 +3,27 @@ package com.exemplos2.exemplomatriz;
 import javax.swing.*;
 
 public class MetodosMatrices {
-
     int [][] notas;
+    String []ListaNomes=new String[Integer.parseInt(JOptionPane.showInputDialog("Introduce cantidad de alumnos"))];
 
     public int [][] crearMatriz(){
-    //Utilizar libreria creada
-    int fila = Integer.parseInt(JOptionPane.showInputDialog("Introduce o numero de filas"));
-    int columna = Integer.parseInt(JOptionPane.showInputDialog("Introduce o numero de columna"));
-    notas=new int[fila][columna];
-    for (int i=0;i<fila;i++) {
-        for (int j = 0; j < columna; j++) {
-        notas[i][j]=Integer.parseInt(JOptionPane.showInputDialog("Introduce a nota"));
+        //Utilizar libreria creada
+        int fila = Integer.parseInt(JOptionPane.showInputDialog("Introduce o numero de filas"));
+        int columna = Integer.parseInt(JOptionPane.showInputDialog("Introduce o numero de columna"));
+        notas=new int[fila][columna];
+        for (int i=0;i<fila;i++) {
+            for (int j = 0; j < columna; j++) {
+                notas[i][j]=Integer.parseInt(JOptionPane.showInputDialog("Introduce a nota"));
+            }
         }
-    }
-    return notas;
+        return notas;
     }
 
     public void amosar(int[][]taboa){
+        crearListaNomes();
         for (int i=0; i<taboa.length;i++){
             System.out.println("\n");
+            System.out.print(ListaNomes[i]+"   ");
             for (int j=0; j<taboa[i].length;j++){
                 System.out.print(taboa[i][j]+"  ");
             }
@@ -30,7 +32,7 @@ public class MetodosMatrices {
     public void notaMediaAlumno(int[][]taboa){
         int acumulador=0;
         for (int i =0; i<taboa.length;i++){
-             acumulador=0;
+            acumulador=0;
             for (int j=0;j<taboa[i].length;j++){
                 acumulador=acumulador+taboa[i][j];
             }
@@ -38,15 +40,35 @@ public class MetodosMatrices {
         }
 
     }
-    public void mediaModulo(int[][]taboa){
-        int acumulador=0;
-        for (int i=0; i<taboa.length;i++){
-            acumulador=0;
-            for (int j=0; j<taboa[i].length;j++){
-                acumulador=acumulador+taboa[j][i];
+    public void mediaModulo(int[][]taboa) {
+        int acumulador = 0;
+        for (int columna = 0; columna < taboa.length; columna++) {
+            acumulador = 0;
+            for (int fila = 0; fila < taboa[columna].length; fila++) {
+                acumulador = acumulador + taboa[fila][columna];
             }
-            System.out.println("\n"+acumulador/taboa[i].length);
+            System.out.println("\n" + acumulador / taboa.length);
         }
+
+       /*
+                         for (int i =0; i<taboa.length;i++){
+                         acumulador=0;
+                         for (int j=0;j<taboa[i].length;j++){
+                         acumulador=acumulador+taboa[j][i];
+                         }
+                         System.out.println("**"+acumulador/taboa.length); */
     }
+
+    public void ordenaNotas(int[][]taboa,String[] listaNomes, int[][]notasMedias){
+
+    }
+
+    public void crearListaNomes(){
+        for (int i=0; i< ListaNomes.length;i++){
+            ListaNomes[i]=JOptionPane.showInputDialog("Introduce o nome do alumno :");
+        }
+
+    }
+
 
 }
